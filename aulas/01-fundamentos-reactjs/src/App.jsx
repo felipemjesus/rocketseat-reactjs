153a3127f6dfafc54a2d3e321b9ceabd80897e57
Felipe Martins <felipemjesus@outlook.com>
2025-03-08 22:23:43 -0300

    Aula - Componente: Sidebar

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,18 +1,27 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./Post"
+import styles from  './App.module.css'
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Fulano da Silva"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, beatae error dolorem vel quam deleniti voluptatem laboriosam sed atque dignissimos, rerum tempore! Eum illum vero nemo dolore obcaecati nihil minus!"
-      />
-      <Post
-        author="Ciclano da Silva"
-        content="Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, beatae error dolorem vel quam deleniti voluptatem laboriosam sed atque dignissimos, rerum tempore! Eum illum vero nemo dolore obcaecati nihil minus!"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Fulano da Silva"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, beatae error dolorem vel quam deleniti voluptatem laboriosam sed atque dignissimos, rerum tempore! Eum illum vero nemo dolore obcaecati nihil minus!"
+          />
+          <Post
+            author="Ciclano da Silva"
+            content="Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, beatae error dolorem vel quam deleniti voluptatem laboriosam sed atque dignissimos, rerum tempore! Eum illum vero nemo dolore obcaecati nihil minus!"
+          />
+        </main>
+      </div>
     </>
   )
 }
