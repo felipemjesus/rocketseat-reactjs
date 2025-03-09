a8067a86adc00a4c938ca5011cb2ae3a997c302e
Felipe Martins <felipemjesus@outlook.com>
2025-03-08 22:39:18 -0300

    Aula - Finalizando Sidebar

--- aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -6,13 +7,15 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/fulanodasilva.png" />
                 <strong>Fulano da Silva</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
-                    <p>Editar seu perfil</p>
+                    <PencilLine size={20} />
+                    Editar seu perfil
                 </a>
             </footer>
         </aside>
