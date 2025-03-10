279ac07629451feb93acee2e20d257b9c1eb992a
Felipe Martins <felipemjesus@outlook.com>
2025-03-09 22:32:39 -0300

    Aula - Componente: Avatar

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img src="https://github.com/fulanodasilva.png" />
+                    <Avatar src="https://github.com/fulanodasilva.png" />
                     <div className={styles.authorInfo}>
                         <strong>Fulano da Silva</strong>
                         <span>Web Developer</span>
