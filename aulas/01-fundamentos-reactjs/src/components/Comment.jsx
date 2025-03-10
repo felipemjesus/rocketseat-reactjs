79c8d554ae4255fedade27ae706726668d055330
Felipe Martins <felipemjesus@outlook.com>
2025-03-09 22:01:58 -0300

    Aula - Componente: Comment

---
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/fulanodasilva.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Fulano da Silva</strong>
+                            <time title="08 de Março às 22:47" dateTime="2025-03-08 22:47:30">Cerca de 1h atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
