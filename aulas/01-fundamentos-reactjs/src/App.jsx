8ee5faa5b25f2b66a368af5ecf2fb3ca6cc852d4
Felipe Martins <felipemjesus@outlook.com>
2025-03-11 21:53:42 -0300

    Aula - Iterando no JSX

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -3,6 +3,37 @@ import { Sidebar } from "./components/Sidebar"
 import { Post } from "./components/Post"
 import styles from  './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/fulanodasilva.png',
+      name: 'Fulano da Silva',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-11 20:41:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/cicranodasilva.png',
+      name: 'Cicrano da Silva',
+      role: 'Mobile Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-01 18:30:00')
+  }
+]
+
 export function App() {
   return (
     <>
@@ -12,8 +43,16 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
