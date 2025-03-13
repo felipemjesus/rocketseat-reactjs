3bde394032f43867678c14bcafd5f2b75a25905e
Felipe Martins <felipemjesus@outlook.com>
2025-03-12 22:23:22 -0300

    Aula - Validando formulário

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -33,7 +33,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange(event) {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid(event) {
+        event.target.setCustomValidity('Preencha o campo com seu comentário')
     }
 
     function deleteComment(commentToDelete) {
@@ -43,6 +48,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne)
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -82,10 +89,14 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
