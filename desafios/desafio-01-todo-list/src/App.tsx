28b3bfc030f9bb31d49b5f8223f1e9331a88e76c
Felipe Martins <felipemjesus@outlook.com>
2025-03-15 09:54:54 -0300

    Desafio - Todo List

--- desafios/desafio-01-todo-list/src/App.tsx
+++ desafios/desafio-01-todo-list/src/App.tsx
@@ -1,31 +1,84 @@
 import { Header } from "./components/Header";
 import styles from './App.module.css'
 import { ClipboardText, PlusCircle, Trash } from "phosphor-react";
-
-const todos = [
-  {
-    title: 'Estudar React.js',
-    finished: false
-  },
-  {
-    title: 'Estudar Node.js',
-    finished: false
-  },
-  {
-    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    finished: false
-  }
-]
+import { ChangeEvent, InvalidEvent, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: 'Estudar React.js',
+      finished: false
+    },
+    {
+      title: 'Estudar Node.js',
+      finished: true
+    },
+  ])
+
+  const [newTodoTitle, setNewTodoTitle] = useState('')
+
+  function handleNewTodo(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const newTodo = {
+      title: newTodoTitle,
+      finished: false
+    }
+
+    setTodos([...todos, newTodo])
+    setNewTodoTitle('')
+  }
+
+  function handleNewTodoTitleChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setNewTodoTitle(event.target.value)
+  }
+
+  function handleNewTodoTitleInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.currentTarget.setCustomValidity('Digite o título da tarefa')
+  }
+
+  function handleDeleteTodo(todoToDelete: string) {
+    const todosWithoutDeletedOne = todos.filter(todo => todo.title !== todoToDelete)
+
+    setTodos(todosWithoutDeletedOne)
+  }
+
+  function handleFinishTodo(todoTitle: string) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.title === todoTitle) {
+        return {
+          ...todo,
+          finished: !todo.finished
+        }
+      }
+
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  const createdTodos = todos.length
+
+  const finishedTodos = todos.filter(todo => todo.finished).length
+
   return (
     <>
       <Header />
 
       <main>
         
-        <form className={styles.todoForm}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
+        <form className={styles.todoForm} onSubmit={handleNewTodo}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            name="newTodo"
+            value={newTodoTitle}
+            onChange={handleNewTodoTitleChange}
+                    onInvalid={handleNewTodoTitleInvalid}
+            required
+          />
           <button type="submit">
             Criar
             <PlusCircle />
@@ -36,11 +89,11 @@ function App() {
           <header>
             <div className={styles.createdTasks}>
               Tarefas criadas
-              <span>0</span>
+              <span>{createdTodos}</span>
             </div>
             <div className={styles.finishedTasks}>
               Concluídas
-              <span>0</span>
+              <span>{finishedTodos} de {createdTodos}</span>
             </div>
           </header>
 
@@ -53,10 +106,20 @@ function App() {
           ) : (
             <ul className={styles.todoList}>
               {todos.map(todo => (
-                <li key={todo.title} className={`${styles.todoItem} ${todo.finished ? styles.finished : ''}`}>
-                  <input type="checkbox" />
-                  <span>{todo.title}</span>
-                  <button>
+                <li key={todo.title} className={styles.todoItem}>
+                  <input
+                    type="checkbox"
+                    onChange={() => handleFinishTodo(todo.title)}
+                    checked={todo.finished}
+                  />
+                  
+                  <span className={todo.finished ? styles.finished : ''}>{todo.title}</span>
+
+                  <button
+                    type="button"
+                     title="Deletar tarefa"
+                    onClick={() => handleDeleteTodo(todo.title)}
+                  >
                     <Trash size={24} />
                   </button>
                 </li>
