809dcf3f3a2b4e970de781d19a7e646a74d96010
Felipe Martins <felipemjesus@outlook.com>
2025-03-19 22:36:20 -0300

    Aula - Aprimorando os inputs

--- aulas/02-aprofundando-em-hooks/src/pages/Home/index.tsx
+++ aulas/02-aprofundando-em-hooks/src/pages/Home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="tasks-suggestions"
           />
+
+          <datalist id="tasks-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
