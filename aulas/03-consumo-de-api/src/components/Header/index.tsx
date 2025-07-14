9f15f8c832901ddc371cce20f6b36ad930fcb0f4
Felipe Martins <felipemjesus@outlook.com>
2025-07-13 21:04:58 -0300

    Aula - Criando um modal acessível

--- aulas/03-consumo-de-api/src/components/Header/index.tsx
+++ aulas/03-consumo-de-api/src/components/Header/index.tsx
@@ -1,16 +1,32 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from "../../assets/logo.svg";
 
 export function Header() {
   return (
     <HeaderContainer>
         <HeaderContent>
-            <img src={logoImg} alt="dt money" />
-            <NewTransactionButton>
-                Nova transação
-            </NewTransactionButton>
-        </HeaderContent>    
+          <img src={logoImg} alt="dt money" />
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay />
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <form>
+                  <input type="text" placeholder="Título" required />
+                  <input type="number" placeholder="Valor" required />
+                  <input type="text" placeholder="Categoria" required />
+                  <button type="submit">Cadastrar</button>
+                </form>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+        </HeaderContent>
     </HeaderContainer>
   );
 }
