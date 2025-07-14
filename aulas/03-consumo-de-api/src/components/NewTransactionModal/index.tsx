7ead9c0ea6876b875222ff20f0097146bebc2dec
Felipe Martins <felipemjesus@outlook.com>
2025-07-13 21:39:05 -0300

    Aula - Criando radio button acessível

--- aulas/03-consumo-de-api/src/components/NewTransactionModal/index.tsx
+++ aulas/03-consumo-de-api/src/components/NewTransactionModal/index.tsx
@@ -19,12 +19,12 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Categoria" required />
 
           <TransactionType>
-            <TransactionTypeButton variant="income">
+            <TransactionTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
               <span>Entrada</span>
             </TransactionTypeButton>
 
-            <TransactionTypeButton variant="outcome">
+            <TransactionTypeButton variant="outcome" value="outcome">
               <ArrowCircleUp size={24} />
               <span>Saída</span>
             </TransactionTypeButton>
