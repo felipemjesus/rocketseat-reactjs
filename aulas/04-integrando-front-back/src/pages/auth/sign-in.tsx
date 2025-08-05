2090b23858300e9c322bbcb227c6666990daea7d
Felipe Martins <felipemjesus@outlook.com>
2025-08-04 21:35:50 -0300

    Aula - Usando React Hook Form

--- aulas/04-integrando-front-back/src/pages/auth/sign-in.tsx
+++ aulas/04-integrando-front-back/src/pages/auth/sign-in.tsx
@@ -1,8 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type { Sign } from "crypto";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SingInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SingInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
     <div className="p-8">
       <div className="w-[350px] flex flex-col justify-center gap-4">
@@ -11,13 +28,13 @@ export function SignIn() {
           <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</p>
         </div>
 
-        <form className="flex flex-col gap-4">
+        <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
           <div className="space-y-2">
             <Label htmlFor="email">E-mail</Label>
-            <Input type="email" id="email" />
+            <Input type="email" id="email" {...register('email')} />
           </div>
 
-          <Button type="submit">
+          <Button type="submit" className="w-full cursor-pointer" disabled={isSubmitting}>
             Acessar painel
           </Button>
         </form>
