c068ef7b471a9a540530d1bbf60f96b4aa262218
Felipe Martins <felipemjesus@outlook.com>
2025-08-04 22:16:01 -0300

    Aula - Página: Cadastro de restaurante

--- aulas/04-integrando-front-back/src/pages/auth/sign-in.tsx
+++ aulas/04-integrando-front-back/src/pages/auth/sign-in.tsx
@@ -1,19 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
 
 const signInForm = z.object({
   email: z.string().email(),
 });
 
-type SingInForm = z.infer<typeof signInForm>;
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SingInForm>()
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -30,10 +34,20 @@ export function SignIn() {
 
   return (
     <div className="p-8">
+      <Button variant="ghost" asChild className="absolute top-8 right-8">
+        <Link to="/sign-up">
+          Novo estabelecimento
+        </Link>
+      </Button>
+
       <div className="w-[350px] flex flex-col justify-center gap-4">
         <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
-          <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</p>
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
         </div>
 
         <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
---
+++ aulas/04-integrando-front-back/src/pages/auth/sign-up.tsx
@@ -0,0 +1,92 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
+import { Link, useNavigate } from "react-router-dom";
+
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+});
+
+type SignUpForm = z.infer<typeof signUpForm>;
+
+export function SignUp() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(data);
+
+    toast.success("Restaurante cadastrado com sucesso!", {
+      action: {
+        label: "Login",
+        onClick: () => navigate('/sign-in'),
+      }
+    });
+  }
+
+  return (
+    <div className="p-8">
+      <Button variant="ghost" asChild className="absolute top-8 right-8">
+        <Link to="/sign-in">
+          Fazer login
+        </Link>
+      </Button>
+
+      <div className="w-[350px] flex flex-col justify-center gap-4">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Criar conta grátis
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Seja um parceiro e comece suas vendas!
+          </p>
+        </div>
+
+        <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+          <div className="space-y-2">
+            <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
+            <Input type="text" id="restaurantName" {...register('restaurantName')} />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="managerName">Seu nome</Label>
+            <Input type="text" id="managerName" {...register('managerName')} />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="phone">Seu celular</Label>
+            <Input type="phone" id="phone" {...register('phone')} />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input type="email" id="email" {...register('email')} />
+          </div>
+
+          <Button type="submit" className="w-full cursor-pointer" disabled={isSubmitting}>
+            Finalizar cadastro
+          </Button>
+
+          <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+            Ao continuar você concorda com nossos {' '}
+            <a href="#" className="underline underline-offset-4">termos de serviço</a> e {' '}
+            <a href="#" className="underline underline-offset-4">políticas de privacidade</a>.
+          </p>
+        </form>
+      </div>
+    </div>
+  )
+}
