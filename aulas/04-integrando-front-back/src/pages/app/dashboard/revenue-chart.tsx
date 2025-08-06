5c43425b766f9338af5e3bc002dc76cb3a499b5b
Felipe Martins <felipemjesus@outlook.com>
2025-08-05 22:47:36 -0300

    Aula - Gráfico de receita no período

---
+++ aulas/04-integrando-front-back/src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,65 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ResponsiveContainer, LineChart, XAxis, YAxis, Line, Tooltip } from "recharts"
+import colors from "tailwindcss/colors";
+
+const data = [
+  { date: '01/07', revenue: 1200 },
+  { date: '02/07', revenue: 2100 },
+  { date: '03/07', revenue: 800 },
+  { date: '04/07', revenue: 1600 },
+  { date: '05/07', revenue: 900 },
+  { date: '06/07', revenue: 1700 },
+  { date: '07/07', revenue: 2500 },
+];
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-semibold">
+            Receita no período
+          </CardTitle>
+          <CardDescription>
+            Receita diária no período
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart
+            style={{ fontSize: 12 }}
+            margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+            data={data}
+          >
+            <Line
+              dataKey="revenue"
+              type="linear"
+              strokeWidth={2}
+              stroke={colors.violet['500']}
+            />
+            <XAxis
+              dataKey="date"
+              axisLine={false}
+              tickLine={false}
+              dy={16}
+            />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                })
+              }
+            />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
