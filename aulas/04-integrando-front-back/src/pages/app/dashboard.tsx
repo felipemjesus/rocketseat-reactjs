e3d79289f02c9c8bb74f509c4cd4223eea81244f
Felipe Martins <felipemjesus@outlook.com>
2025-08-05 22:24:29 -0300

    Aula - Página: Dashboard

--- aulas/04-integrando-front-back/src/pages/app/dashboard.tsx
+++
@@ -1,7 +0,0 @@
-export function Dashboard() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-    </div>
-  )
-}
