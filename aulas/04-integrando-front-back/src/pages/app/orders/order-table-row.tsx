2fd8053cad7ea8f93cab2bf15857793a33586655
Felipe Martins <felipemjesus@outlook.com>
2025-08-07 22:09:48 -0300

    Aula - Cancelar pedido

--- aulas/04-integrando-front-back/src/pages/app/orders/order-table-row.tsx
+++ aulas/04-integrando-front-back/src/pages/app/orders/order-table-row.tsx
@@ -7,6 +7,9 @@ import { OrderStatus } from "@/components/order-status";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { cancelOrder } from "@/api/cancel-order";
+import type { GetOrdersResponse } from "@/api/get-orders";
 
 export interface OrderTableRowProps {
   order: {
@@ -20,6 +23,36 @@ export interface OrderTableRowProps {
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
   const [isDatailsOpen, setIsDatailsOpen] = useState(false);
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: orderCancel } = useMutation({
+    mutationFn: cancelOrder,
+    onSuccess: (_, { orderId }) => {
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: ["orders"],
+      });
+
+      ordersListCache.forEach(([cacheKey, cacheData]) => {
+        if (!cacheData) {
+          return;
+        }
+
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+          ...cacheData,
+          orders: cacheData.orders.map((order) => {
+            if (order.orderId === orderId) {
+              return {
+                ...order,
+                status: "canceled",
+              };
+            }
+
+            return order;
+          }),
+        });
+      });
+    }
+  })
 
   return (
     <TableRow>
@@ -63,7 +96,12 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         </Button>
       </TableCell>
       <TableCell className="text-right">
-        <Button variant="ghost" size="sm">
+        <Button
+          variant="ghost"
+          size="sm"
+          disabled={!['pending', 'processing'].includes(order.status)}
+          onClick={() => orderCancel({ orderId: order.orderId })}
+        >
           <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>
