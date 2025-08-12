c416d53e07f554fe1a6131a7e992bb4210dfdced
Felipe Martins <felipemjesus@outlook.com>
2025-08-11 22:06:13 -0300

    Aula - Estrutura da Home

--- aulas/05-primeiro-framework/src/pages/index.tsx
+++ aulas/05-primeiro-framework/src/pages/index.tsx
@@ -1,26 +1,30 @@
-import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '1rem 2rem',
-
-  span: {
-    color: '#fff',
-    marginLeft: '0.5rem'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.9)'
-  }
-})
+import camiseta1 from "@/assets/camisetas/1.png";
+import camiseta2 from "@/assets/camisetas/2.png";
+import camiseta3 from "@/assets/camisetas/3.png";
 
 export default function Home() {
   return (
-    <Button>
-      Enviar
-      <span>teste</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={528} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
