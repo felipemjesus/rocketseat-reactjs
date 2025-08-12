2c39c88b5b81d709cc7532c33450d6c799af914f
Felipe Martins <felipemjesus@outlook.com>
2025-08-11 21:31:18 -0300

    Aula - Cabeçalho da aplicação

--- aulas/05-primeiro-framework/src/pages/_app.tsx
+++ aulas/05-primeiro-framework/src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import { Roboto } from 'next/font/google'
+import Image from "next/image";
+import { globalStyles } from "@/styles/global";
+import logoImg from "@/assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -10,5 +13,13 @@ const roboto = Roboto({
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className={roboto.className} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Logo" />
+      </Header>
+
+      <Component {...pageProps} className={roboto.className} />
+    </Container>
+  )
 }
