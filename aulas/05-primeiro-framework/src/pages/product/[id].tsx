244f3e69cfbbc7c715699f106d50d0d0c2a7d1c6
Felipe Martins <felipemjesus@outlook.com>
2025-08-13 22:46:36 -0300

    Aula - Carregando dados do produto

--- aulas/05-primeiro-framework/src/pages/product/[id].tsx
+++ aulas/05-primeiro-framework/src/pages/product/[id].tsx
@@ -1,20 +1,31 @@
+import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
-import { useRouter } from "next/router";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import Stripe from "stripe";
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
 
+export default function Product(props: ProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={props.product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{props.product.name}</h1>
+        <span>{props.product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod</p>
+        <p>{props.product.description}</p>
 
         <button>
           Comprar agora
@@ -22,4 +33,32 @@ export default function Product() {
       </ProductDetails>
     </ProductContainer>
   );
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const productId = String(params?.id)
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  const amount = price.unit_amount || 0
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1,  // 1 hour
+  }
 }
