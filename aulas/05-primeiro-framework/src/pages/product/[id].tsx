cd760772855a654b65bc0eb910d849b40041ec7e
Felipe Martins <felipemjesus@outlook.com>
2025-08-13 22:34:04 -0300

    Aula - Estrutura da página de produto

--- aulas/05-primeiro-framework/src/pages/product/[id].tsx
+++ aulas/05-primeiro-framework/src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product { JSON.stringify(query) }</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
