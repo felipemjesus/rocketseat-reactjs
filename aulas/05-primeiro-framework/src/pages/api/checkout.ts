e3a62ad11ef1053aaf943ec5b14eaf11800bfae3
Felipe Martins <felipemjesus@outlook.com>
2025-08-14 21:51:10 -0300

    Aula - Dados da compra no sucesso

--- aulas/05-primeiro-framework/src/pages/api/checkout.ts
+++ aulas/05-primeiro-framework/src/pages/api/checkout.ts
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Product not found' })
   }
 
-  const successUrl = `${process.env.NEXT_APP_URL}/success`
+  const successUrl = `${process.env.NEXT_APP_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_APP_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
