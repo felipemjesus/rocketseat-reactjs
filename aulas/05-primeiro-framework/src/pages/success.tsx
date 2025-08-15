15ff9e58f5a90c34fdc811be85f96824ddeb0148
Felipe Martins <felipemjesus@outlook.com>
2025-08-14 21:35:02 -0300

    Aula - Estrutura de sucesso

--- aulas/05-primeiro-framework/src/pages/success.tsx
+++ aulas/05-primeiro-framework/src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Felipe Martins</strong>, sua <strong>Camiseta Beyond the limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catalogo
+      </Link>
+    </SuccessContainer>
   );
 }
