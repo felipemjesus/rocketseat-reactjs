166706dde6733765550924df06d149dab26724cb
Felipe Martins <felipemjesus@outlook.com>
2025-08-20 22:15:31 -0300

    Aula - Documentação de cores

--- aulas/06-design-system/packages/docs/.storybook/preview.ts
+++ aulas/06-design-system/packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react-vite";
+import { themes } from "storybook/internal/theming";
 
 const preview: Preview = {
   parameters: {
@@ -14,6 +15,10 @@ const preview: Preview = {
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
       test: "todo",
+    },
+
+    docs: {
+      theme: themes.dark,
     },
   },
 };
