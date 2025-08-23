d06a61990c408160fa21078ae1eaf42d85c825e6
Felipe Martins <felipemjesus@outlook.com>
2025-08-23 13:46:39 -0300

    Aula - Ajustes no Design System

--- aulas/06-design-system/packages/react/src/components/Button.tsx
+++ aulas/06-design-system/packages/react/src/components/Button.tsx
@@ -23,6 +23,10 @@ export const Button = styled('button', {
     opacity: 0.6,
   },
 
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$ignite300',
+  },
+
   svg: {
     width: '$4',
     height: '$4',
