9f9eac300a61fc0bc2bc2440f76f5fbb8b825fac
Felipe Martins <felipemjesus@outlook.com>
2025-08-23 13:03:15 -0300

    Aula - Configurando Changesets

--- aulas/06-design-system/packages/docs/src/stories/Box.stories.tsx
+++ aulas/06-design-system/packages/docs/src/stories/Box.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, Text, type BoxProps } from '@ignite-ui/react'
+import { Box, Text, type BoxProps } from '@felipemjesus-ignite-ui/react'
 
 export default {
   title: 'Surfaces/Box',
