7aaea0f68fa424be06d9b6a4d95f7cc8ac38884a
Felipe Martins <felipemjesus@outlook.com>
2025-08-23 20:00:36 -0300

    Aula - Página de conexão do calendário

---
+++ aulas/07-aprofundando-em-nextjs/src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,35 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ArrowRight } from 'phosphor-react'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function ConnectCalendar() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2}></MultiStep>
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button>
+          Proximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
