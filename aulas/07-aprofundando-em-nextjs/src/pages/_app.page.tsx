fdecc2bedbf5a963ee57c9d4d55f347f37bade38
Felipe Martins <felipemjesus@outlook.com>
2025-08-24 11:38:54 -0300

    Aula - Configurando o Next Auth

--- aulas/07-aprofundando-em-nextjs/src/pages/_app.page.tsx
+++ aulas/07-aprofundando-em-nextjs/src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import { SessionProvider } from 'next-auth/react'
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
@@ -9,6 +10,13 @@ const roboto = Roboto({
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className={roboto.className} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} className={roboto.className} />
+    </SessionProvider>
+  )
 }
--- aulas/07-aprofundando-em-nextjs/src/pages/register/connect-calendar/index.page.tsx
+++ aulas/07-aprofundando-em-nextjs/src/pages/register/connect-calendar/index.page.tsx
@@ -2,8 +2,13 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function ConnectCalendar() {
+  const session = useSession()
+
+  const isConnected = session.status === 'authenticated'
+
   return (
     <Container>
       <Header>
@@ -19,8 +24,13 @@ export default function ConnectCalendar() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
-            Conectar
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+            disabled={isConnected}
+          >
+            {isConnected ? 'Conectado' : 'Conectar'}
             <ArrowRight />
           </Button>
         </ConnectItem>
