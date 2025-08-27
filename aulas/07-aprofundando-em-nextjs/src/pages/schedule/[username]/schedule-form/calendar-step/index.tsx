143bc6a5c439b7d00cc215cd8beab266ed98ab84
Felipe Martins <felipemjesus@outlook.com>
2025-08-27 20:28:13 -0300

    Aula - Tabela de agendamentos

--- aulas/07-aprofundando-em-nextjs/src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
+++ aulas/07-aprofundando-em-nextjs/src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
@@ -7,11 +7,18 @@ import {
   TimePickerList,
 } from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export default function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -20,7 +27,7 @@ export default function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            segunda-feira, <span>25 de agosto</span>
+            {weekDay}, <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
